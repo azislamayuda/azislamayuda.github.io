8eae99dc2d65594107d5d6efa71d4b6326392397
MJclopedia <azis.lamayuda89@gmail.com>
2024-04-22 11:47:25 +0700

    change menu bar to awezis

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@
 
   on("click", ".mobile-nav-toggle", function (e) {
     select("#navbar").classList.toggle("navbar-mobile");
-    this.classList.toggle("fa-circle-ellipsis");
+    this.classList.toggle("fa-bars");
     this.classList.toggle("fa-xmark");
   });
 
