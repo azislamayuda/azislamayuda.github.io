023c5cc410c3556fc37234bd58e78ce9af033b0e
AZIS LAMAYUDA <azis.lamayuda89@gmail.com>
2026-07-19 06:58:37 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Personal
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
